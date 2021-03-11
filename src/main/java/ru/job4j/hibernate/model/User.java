a19294fe3c536e659d186d4db8c5254e7fe0c1f3
grigan-Uragan <prischepny@mail.ru>
2021-03-11 16:26:25 +0300

    start poject

---
+++ src/main/java/ru/job4j/hibernate/model/User.java
@@ -0,0 +1,51 @@
+package ru.job4j.hibernate.model;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+
+    private Role role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
