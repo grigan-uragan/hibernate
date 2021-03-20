38efc144f6da194b113390c68169a93fa91f7c30
grigan-Uragan <prischepny@mail.ru>
2021-03-20 23:45:44 +0300

    4. Время жизни (Scope) [#469366]

--- src/main/java/ru/job4j/spring/di/SpringMain.java
+++ src/main/java/ru/job4j/spring/di/SpringMain.java
@@ -8,8 +8,13 @@ public class SpringMain {
         context.scan("ru.job4j.spring.di");
         context.refresh();
         StartUI startUI = context.getBean(StartUI.class);
-        startUI.add();
-        startUI.add();
-        startUI.print();
+        StartUI another = context.getBean(StartUI.class);
+        ConsoleInput input = context.getBean(ConsoleInput.class);
+        ConsoleInput anotherInput = context.getBean(ConsoleInput.class);
+
+        System.out.println("StartUI has scope prototype, startUI == another ? "
+                + (startUI == another));
+        System.out.println("Console input has default scope singleton, input == anotherInput? "
+                + (input == anotherInput));
     }
 }
